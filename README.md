0c7d6f8427664858d311a2db40aace5c270dcc94
Peyton Vaughn <pvaughn@6fusion.com>
2017-05-01 18:01:05 -0400

    initial checkin

---
+++ README.md
@@ -0,0 +1,4 @@
+# lita-meter-sidekick
+
+Lita ChatOps handler for 6fusion Meter CI/CD operations
+
---
+++ lib/lita/handlers/meter_sidekick.rb
@@ -0,0 +1,34 @@
+module Lita
+  module Handlers
+    class MeterSidekick < Handler
+
+      METERCTL_BUCKET='6fusion-meter-dev'
+
+      route(/meter latest/, :latest, help: { 'meter latest' => 'Latest meter installers' })
+
+
+
+      def latest(response)
+        s3 = Aws::S3::Client.new
+        bucket = Clients.s3.list_objects_v2(bucket: METERCTL_BUCKET)
+        coreos = bucket.contents.select{|entry| entry.key.match(%r|coreos/.+/install|)}
+        stable = coreos
+                   .reject{|entry| entry.match(/alpha|beta/)}
+                   .sort{|a,b| Gem::Version.new(a) <=> Gem::Version.new(b) }
+                   .first
+                   .key
+        beta = coreos
+                 .select{|entry| entry.end_with?('beta')}
+                 .sort{|a,b| Gem::Version.new(a) <=> Gem::Version.new(b) }
+                 .first
+                 .key
+
+        # FIXME how do you get this from the SDK
+        url_base = 'https://s3.amazonaws.com/6fusion-meter-dev/'
+        response.reply("#{url_base}/#{stable}\n#{url_base}/#{beta}")
+      end
+
+      Lita.register_handler(self)
+    end
+  end
+end
