125b1cab7980f861f4846984d741064e2145cf3f
Peyton Vaughn <pvaughn@6fusion.com>
2017-05-03 09:19:40 -0400

    ec2 instances wip

---
+++ lib/lita-meter-sidekick/ec2.rb
@@ -0,0 +1,20 @@
+require 'aws-sdk'
+
+module LitaMeterSidekick
+  module EC2
+
+    def list_deployed_meters
+      begin
+        regions.each do |region|
+          ec2 = Aws::EC2::Resource.new(region: region)
+          meter_instances = ec2.instances(filters: [{ name: 'tag:ApplicationRole', values:['6fusionMeter'] }])
+          response.reply(render_template('instance_list', instances: meter_instances))
+      end
+    end
+
+    def regions
+      @regions ||= Clients.ec2.describe_regions.data.regions.map(&:region_name)
+    end
+
+  end
+end
