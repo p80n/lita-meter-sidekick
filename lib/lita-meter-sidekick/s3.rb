7e96ccea4ffae5d4f1632a0b87520c27ef0cc086
Peyton Vaughn <pvaughn@6fusion.com>
2017-05-03 09:15:35 -0400

    ec2 instances wip

--- lib/lita-meter-sidekick/s3.rb
+++ lib/lita-meter-sidekick/s3.rb
@@ -6,30 +6,23 @@ module LitaMeterSidekick
     METERCTL_BUCKET='6fusion-meter-dev'
 
     def latest(response)
-      begin
-        s3 = Aws::S3::Client.new
-        bucket = s3.list_objects_v2(bucket: METERCTL_BUCKET)
-        coreos = bucket.contents.select{|entry| entry.key.match(%r|coreos/.+/install|)}
-        stable = coreos
-                   .map{|e| e.key.split('/')[1]}
-                   .reject{|entry| entry.match(/alpha|beta/)}
-                   .sort{|a,b| Gem::Version.new(b) <=> Gem::Version.new(a) }
-                   .first
-        beta = coreos
+      s3 = Aws::S3::Client.new
+      bucket = s3.list_objects_v2(bucket: METERCTL_BUCKET)
+      coreos = bucket.contents.select{|entry| entry.key.match(%r|coreos/.+/install|)}
+      stable = coreos
                  .map{|e| e.key.split('/')[1]}
-                 .select{|entry| entry.end_with?('beta')}
+                 .reject{|entry| entry.match(/alpha|beta/)}
                  .sort{|a,b| Gem::Version.new(b) <=> Gem::Version.new(a) }
                  .first
+      beta = coreos
+               .map{|e| e.key.split('/')[1]}
+               .select{|entry| entry.end_with?('beta')}
+               .sort{|a,b| Gem::Version.new(b) <=> Gem::Version.new(a) }
+               .first
 
-        # FIXME how do you get this from the SDK
-        url_base = 'https://s3.amazonaws.com/6fusion-meter-dev/coreos'
-        response.reply(render_template('installer_links', stable: "#{url_base}/#{stable}/install", beta: "#{url_base}/#{beta}/install"))
-      rescue => e
-        response.reply(render_template('exception', exception: e))
-        log e.message
-        log e.backtrace.join("\n")
-      end
+      # FIXME how do you get this from the SDK
+      url_base = 'https://s3.amazonaws.com/6fusion-meter-dev/coreos'
+      response.reply(render_template('installer_links', stable: "#{url_base}/#{stable}/install", beta: "#{url_base}/#{beta}/install"))
     end
-    
   end
 end
--- lib/lita/handlers/meter_sidekick.rb
+++ lib/lita/handlers/meter_sidekick.rb
@@ -3,9 +3,18 @@ module Lita
     class MeterSidekick < Handler
       include ::LitaMeterSidekick::S3
 
-      route(/meter latest/, :latest, help: { 'meter latest' => 'Links to installers for latest version of the meter' })
+      begin
+        route(/meter latest/, :latest, help: { 'meter latest' => 'Links to installers for latest version of the Meter' })
 
-      Lita.register_handler(self)
+        route(/meter deployed/, :list_deployed_meters, help: { 'meter deployed' => 'List all discoverable instances of the Meter in EC2' })
+
+        Lita.register_handler(self)
+
+      rescue => e
+        response.reply(render_template('exception', exception: e))
+        log e.message
+        log e.backtrace.join("\n")
+      end
     end
   end
 end
