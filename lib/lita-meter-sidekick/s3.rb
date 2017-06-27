0faa263057e09d46878ec2393dbf24827624cc22
Peyton Vaughn <pvaughn@6fusion.com>
2017-06-27 14:42:33 -0400

    handle busted resources

--- lib/lita-meter-sidekick/s3.rb
+++ lib/lita-meter-sidekick/s3.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'net/http'
 
 module LitaMeterSidekick
   module S3
@@ -9,6 +10,7 @@ module LitaMeterSidekick
       s3 = Aws::S3::Client.new
       bucket = s3.list_objects_v2(bucket: METERCTL_BUCKET)
       coreos = bucket.contents.select{|entry| entry.key.match(%r|coreos/.+/install|)}
+      warning = nil
       stable = coreos
                  .map{|e| e.key.split('/')[1]}
                  .reject{|entry| entry.match(/alpha|beta/)}
@@ -22,11 +24,31 @@ module LitaMeterSidekick
 
       # FIXME how do you get this from the SDK
       url_base = 'https://s3.amazonaws.com/6fusion-meter-dev/coreos'
-      response.reply(render_template('installer_links',
-                                     stable: "#{url_base}/#{stable}/install",
-                                     beta: beta.match(/#{stable}-beta/) ? 'There is no beta release currently in the works' : "#{url_base}/#{beta}/install",
-                                     alpha: "#{url_base}/alpha/install"))
 
+      stable_link = "#{url_base}/#{stable}/install"
+      unless resources_updated_for?(version)
+        stable_link = "~#{stable_link}~"
+        warning = "*Note:* resources.6fusion.com not updated. Stable installer link will not work."
+      end
+
+      beta_link = beta.match(/#{stable}-beta/) ?
+                    'There is no beta release currently in the works' :
+                    "#{url_base}/#{beta}/install",
+
+      response.reply(render_template('installer_links',
+                                     stable: stable_link,
+                                     beta:   beta_link,
+                                     alpha:  "#{url_base}/alpha/install",
+                                     warning: warning))
+
+    end
+
+    private
+    def resources_updated_for?(version)
+      url = URI.parse("https://resources.6fusion.com/meter/coreos/v#{version}/meterctl")
+      request = Net::HTTP.new(url.host, url.port)
+      response = request.request_head(url.path)
+      response.code == '404'
     end
   end
 end
